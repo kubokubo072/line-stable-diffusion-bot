7e5f6ddb702ee5a13a54049e410cb76f41c4341b
久保田空也 <0rb05k1c78v4y5f@gmail.com>
2025-04-03 00:53:15 +0900

    コメントを修正

--- config/main/const.php
+++ config/main/const.php
@@ -1,10 +1,11 @@
 <?php
+// apiのurlを管理します
 return [
     'stabilityApi' => [
         'generate' => 'https://api.stability.ai/v2beta/stable-image/generate/core',
         'sketch' => 'https://api.stability.ai/v2beta/stable-image/control/sketch',
     ],
-    'test' => [
-        'test' => '',
+    'template' => [
+        'template' => '',
     ],
 ];
